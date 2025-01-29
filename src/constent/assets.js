f3fa12587cad0250bef9a98af1d8f4039b1a89e1
Skulldued <skulldued30@gmail.com>
2025-01-30 00:10:45 +0530

    basic setup completed

---
+++ src/constent/assets.js
@@ -0,0 +1,71 @@
+import bg1 from "../assets/bg1.png";
+import blog6 from "../assets/blog6.png";
+import blog7 from "../assets/blog7.png";
+import blog8 from "../assets/blog8.png";
+import ctabg2 from "../assets/cta-bg2.png";
+import heroThumb from "../assets/hero-thumb1.png";
+import icon1 from "../assets/icon1.svg";
+import icon2 from "../assets/icon2.svg";
+import icon3 from "../assets/icon3.svg";
+import icon4 from "../assets/icon4.svg";
+import icon5 from "../assets/icon5.svg";
+import icon6 from "../assets/icon6.svg";
+import icon7 from "../assets/icon7.svg";
+import icon8 from "../assets/icon8.svg";
+import logo from "../assets/logo-dark.svg";
+import p1 from "../assets/p1.png";
+import p2 from "../assets/p2.png";
+import p3 from "../assets/p3.png";
+import p4 from "../assets/p4.png";
+import p5 from "../assets/p5.png";
+import quotes from "../assets/quote.svg";
+import rating from "../assets/rating.svg";
+import shape1 from "../assets/shape1.svg";
+import shape2 from "../assets/shape2.svg";
+import shape3 from "../assets/shape3.svg";
+import shape4 from "../assets/shape4.svg";
+import team1 from "../assets/team1.png";
+import team2 from "../assets/team2.png";
+import team3 from "../assets/team3.png";
+import team4 from "../assets/team4.png";
+import thumb1 from "../assets/thumb1.png";
+import thumb2 from "../assets/thumb2.png";
+import thumb3 from "../assets/thumb3.png";
+import thumb4 from "../assets/thumb4.png";
+
+export const assets = {
+  bg1,
+  blog6,
+  blog7,
+  blog8,
+  ctabg2,
+  heroThumb,
+  icon1,
+  icon2,
+  icon3,
+  icon4,
+  icon5,
+  icon6,
+  icon7,
+  icon8,
+  logo,
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  quotes,
+  rating,
+  shape1,
+  shape2,
+  shape3,
+  shape4,
+  team1,
+  team2,
+  team3,
+  team4,
+  thumb1,
+  thumb2,
+  thumb3,
+  thumb4,
+};
